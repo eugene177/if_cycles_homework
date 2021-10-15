cd75731681398513bfa50ea70d932d9dad1f4fb0
eugene177 <vbxer2019@gmail.com>
2021-10-15 11:51:08 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// // 1. Проверка значения из диапазона
+// let age = 14;
+// if (age<=90 && age >= 14) {
+//   console.log('Значение в диапазоне')
+// } else {
+//   console.log('Значение не в диапазоне')
+// }
+
+// // 2. Проверка значения вне диапазона
+// let age = 13;
+// if (!(age<=90 && age >= 14)) {
+//   console.log('Значение не в диапазоне')
+// } else {
+//   console.log('Значение в диапазоне')
+// }
+
+// // 3. 'if' into '?'
+// let result;
+// const a = 2;
+// const b = 1;
+// (a + b < 4) ? result = 'Few' : result = 'A lot';
+// console.log(result)
+
